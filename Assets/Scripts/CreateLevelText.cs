5907204021a3be39fd3b2371ea7c6c4f855117e3
Salem <selem.menssi.2000@gmail.com>
2022-10-16 04:45:32 +0100

    add color function

--- Assets/Scripts/CreateLevelText.cs
+++ Assets/Scripts/CreateLevelText.cs
@@ -15,9 +15,21 @@ public class CreateLevelText : MonoBehaviour
     [SerializeField]
     private GameObject Selector;
 
+    public GameObject Color;
+
+    [SerializeField]
+    private Button AddColor;
+
     private int activateSelectorIndex = -1;
 
+    private List<string> colorsList = new List<string>
+    {
+        "white","black","red","green","blue"
+    };
+
     private List<GameObject> Selectors = new List<GameObject>();
+
+    private List<Button> elements = new List<Button>();
 
     private List<string> levelOne = new List<string>
     {
@@ -33,10 +45,14 @@ public class CreateLevelText : MonoBehaviour
     {
         createText();
         createSelector();
+        addColorAtribut();
+        
 
     }
+    private void Update()
+    {
+    }
 
-   
 
     public void createText()
     {
@@ -52,6 +68,7 @@ public class CreateLevelText : MonoBehaviour
                 }
                 Selectors[activateSelectorIndex].SetActive(true);
             });
+            elements.Add(element);
         }
     }
     public void createSelector()
@@ -89,9 +106,69 @@ public class CreateLevelText : MonoBehaviour
         //change the text in the element prefab
         TMP_Text className = selector.GetComponentInChildren<TMP_Text>();
         className.text = text;
+        
 
         return selector;
     }
 
-    
+    public void ColorAtrribut(GameObject color)
+    {
+        Dropdown ColorDropdown = color.transform.GetChild(1).GetComponent<Dropdown>();
+        
+
+        ColorDropdown.ClearOptions();
+
+        foreach (string col in colorsList)
+        {
+            ColorDropdown.options.Add(new Dropdown.OptionData() { text = col });
+        }
+        Debug.Log(ColorDropdown.GetInstanceID());
+        TMP_Text value = color.transform.GetChild(0).GetComponent<TMP_Text>();
+        value.text = "color : " + ColorDropdown.options[ColorDropdown.value].text;
+        ColorDropdown.onValueChanged.AddListener(delegate
+        {
+            ChangeColor( ColorDropdown);
+            value.text = "color : " + ColorDropdown.options[ColorDropdown.value].text;
+        });
+        
+    }
+
+    public void ChangeColor(Dropdown color)
+    {
+        if (color.options[color.value].text == "white")
+        {
+            elements[activateSelectorIndex].GetComponentInChildren<TMP_Text>().color = new Color(1f, 1f, 1f);
+        }
+        else if (color.options[color.value].text == "red")
+        {
+            Debug.Log("red");
+            elements[activateSelectorIndex].GetComponentInChildren<TMP_Text>().color = new Color(1f, 0f, 0f);
+        }
+        else if (color.options[color.value].text == "black")
+        {
+            elements[activateSelectorIndex].GetComponentInChildren<TMP_Text>().color = new Color(0f, 0f, 0f);
+        }
+        else if (color.options[color.value].text == "green")
+        {
+            elements[activateSelectorIndex].GetComponentInChildren<TMP_Text>().color = new Color(0f, 1f, 0f);
+        }
+        else if (color.options[color.value].text == "blue")
+        {
+            elements[activateSelectorIndex].GetComponentInChildren<TMP_Text>().color = new Color(0f, 0f, 1f);
+        }
+    } 
+
+    public void addColorAtribut()
+    {
+        AddColor.onClick.AddListener(() =>
+        {
+            GameObject color = Instantiate(Color);
+            VerticalLayoutGroup content = Selectors[activateSelectorIndex].GetComponentInChildren<VerticalLayoutGroup>();
+            color.transform.SetParent(content.transform, false);
+
+            ColorAtrribut(color);
+
+            
+        });
+    }
 }
